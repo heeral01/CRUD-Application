3f5b12d33e1ab675f40629dce0b9b182fb7cae17
heeral01 <gourheeral@gamil.com>
2024-06-10 22:51:42 +0530

    Add Navbar in our project

---
+++ client/src/component/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+import './Navbar.css'
+function Navbar() {
+  return (
+     <>
+     <nav className='main-nav'>
+       <div className="logo">
+        <h1><span>C</span>rud-<span>A</span>pplication</h1>
+       </div>
+       <div className="main-menu">
+        <h2>Home</h2>
+       </div>
+       <div className="main-search">
+        <input type="text" placeholder='Search' className='search' />
+        <button type="button"><IoSearch /></button>
+       </div>
+     </nav>
+     </>
+  )
+}
+
+export default Navbar
