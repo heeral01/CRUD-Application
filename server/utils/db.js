a03ce551b2c05be9a6ecd0376d2a65843bfefa07
Heeral Gour <117432070+heeral01@users.noreply.github.com>
2024-06-15 21:12:04 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,5 +4,5 @@ const DB = "mongodb://127.0.0.1:27017/CRUD-APP";
 
 mongoose
   .connect(DB)
-  .then(() => console.log("Connection sussecfully"))
+  .then(() => console.log("Connection successfully"))
   .catch((error) => console.log(error.message));
